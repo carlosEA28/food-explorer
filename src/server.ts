dec837916b88b1b5907ec05224a73f83b258a5e1
carlosEA28 <carloseaduarte28@gmail.com>
2025-03-04 09:57:24 -0300

    adding aws sdk

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "../src/env";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("HTTP Server is running");
+});
