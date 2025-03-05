bbd74ae2f722669213a441c044f2db9b377bac51
carlosEA28 <carloseaduarte28@gmail.com>
2025-03-05 17:55:33 -0300

    adding swagger and cors config

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,37 @@
 import fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { env } from "../src/env";
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+  origin: true,
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Food Explorer",
+      version: "0.0.1",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP Server is running");
