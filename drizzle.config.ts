34823973d70d0bce1faad0062725032f98d35a6f
carlosEA28 <carloseaduarte28@gmail.com>
2025-03-07 13:31:43 -0300

    changing from drizzle to prisma

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config, defineConfig } from "drizzle-kit";
-import { env } from "./src/env";
-
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./src/drizzle/entity/*",
-  out: "./src/drizzle/migrations",
-  dbCredentials: { url: env.POSTGRES_URL },
-} satisfies Config);
